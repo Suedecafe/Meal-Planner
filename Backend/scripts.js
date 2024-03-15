e822bf44b8b999350f1070e205bc20a01fb8dad7
Dante Scott <dantes1670@gmail.com>
2024-03-15 02:24:28 -0400

    Big Commit Dump
    Added the Create Meal Plan page and About Page with changes to CSS for all previously added pages. Also added comments to current versions of scripts.js and styles.css files

--- Backend/scripts.js
+++ Backend/scripts.js
@@ -1,4 +1,18 @@
-function navigateTest() {
+//Function that links to CreateMealPage.html
+function navigateToCreate() {
     
-    window.location.href = 'https://www.google.com';
+    window.location.href = 'CreateMealPage.html';
 }
+// Retrieves a reference to the input element with id caloriesRange
+const caloriesRange = document.getElementById("caloriesRange");
+// Retrieves a reference to the element with id caloriesValue
+const caloriesValue = document.getElementById("caloriesValue");
+
+// Adds event listener to caloriesRange input element
+caloriesRange.addEventListener("input", function() {
+  // Retrieves current value of caloriesRange input element
+  const calories = caloriesRange.value;
+ // Updates text content of caloriesValue element to display the selected calorie value
+  // Concats selected calorie value with the string "calories"
+  caloriesValue.textContent = calories +  " calories";
+});
